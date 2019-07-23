9ff622faea98a6cbf84b46e3e3bf6d7be9c477c6
SW <dagedieb@gmail.com>
2019-07-23 17:20:52 +0200

    Setting port to 8000
    Former-commit-id: 3597aa3a8885a71b9091b5dcb19a61a101c9b1b9

--- human_in_loop/client.py
+++ human_in_loop/client.py
@@ -95,7 +95,7 @@ class Client:
         self.send_message(msg=json.dumps(command))
 
     def connect_to_server(self):
-        reactor.connectTCP('localhost', 8000, EchoClientFactory(app=self.app, client=self))
+        reactor.connectTCP('localhost', 80, EchoClientFactory(app=self.app, client=self))
 
     def on_connection(self, connection):
         self.print_message("Connected successfully!")
