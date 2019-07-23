1ab4a77a16249f5100a6761d9eee1ed36cab8ff1
SW <dagedieb@gmail.com>
2019-07-23 17:03:58 +0200

    Getting sample from server
    Former-commit-id: 790bd129886a1b2aab2f52bc1ce52a35ce648609

--- human_in_loop/sampler.py
+++ human_in_loop/sampler.py
@@ -6,11 +6,24 @@ class Sampler:
     def __init__(self, sample_file):
         self.sample_file = sample_file
         self.htmler = DifferenceBetweenHtmlParser(input_dir, process_dir, output_dir, input_html_file_filter)
+        self.read_library_f = self.start_reading_library()
+
 
     def add_to_library(self, text):
-        with open(self.sample_file, 'a+') as f:
+        with open(self.sample_file, 'a') as f:
             f.write(text)
             f.write('\n')
+
+    def start_reading_library(self):
+        with open(self.sample_file, 'r') as f:
+            content = f.read()
+            text = str(content).split(sep='\n')
+            for line in text:
+                print ('sample:', line)
+                yield line
+
+    def deliver_sample(self):
+        return next(self.read_library_f)
 
     def complicated_sample(self, text):
         with open("./server_complicated.txt", 'a+') as f:
--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 import json
+import pprint
 
 import sys
+import traceback
+
 print(sys.path)
 
 from human_in_loop.corpus import Corpus
@@ -8,7 +11,7 @@ from human_in_loop.model import Model
 from human_in_loop.sampler import Sampler
 
 model = Model(model_path="models/model.tar.gz")
-corpus = Corpus(path='server_annotation_bin.conll')
+corpus = Corpus(path='hil17.conll3')
 sampler = Sampler(sample_file='server_samples_bin.txt')
 
 import regex as re
@@ -19,6 +22,10 @@ routing_protocol_client_to_server = {
         {'fun': lambda x: model.predict(x['text']),
          'answer': 'prediction'
         },
+    'deliver_sample':
+        {'fun': lambda x: sampler.deliver_sample(),
+         'answer': 'got_sample'
+         },
     'save_sample':
         {'fun': lambda x: sampler.add_to_library(x['text'])},
     'save_a_complicated_sample':
@@ -43,7 +50,7 @@ class Echo(protocol.Protocol):
 
     def dataReceived(self, data):
         "As soon as any data is received, write result back."
-        print (data)
+        print ('command was: %s' % (pprint.pformat(data)))
         if '}{' in str(data):
             print ('double command!')
             commands = re.findall(r'({[^{}]+})', str(data))
@@ -51,23 +58,24 @@ class Echo(protocol.Protocol):
                 self.dataReceived(c)
             return
         json_msg = json.loads(data)
-        print(data)
-        print ('command' in json_msg)
-        print ('command' in json_msg and json_msg['command'] in routing_protocol_client_to_server)
+        print ('Is this a json-value?', 'command' in json_msg)
 
-        result = 'None'
         if 'command' in json_msg and json_msg['command'] in routing_protocol_client_to_server:
-            print ('here')
             try:
                 result = routing_protocol_client_to_server[json_msg['command']]['fun'](json_msg)
             except Exception as e:
-                result = str(e) + '\n\n\n BUT SERVER CONTINUES'
+                result = traceback.format_exc() + '\n\n\n BUT SERVER CONTINUES'
             print (result)
             if 'answer' in routing_protocol_client_to_server[json_msg['command']]:
                 result = {
                     'command': routing_protocol_client_to_server[json_msg['command']]['answer'],
                     'result': result
                 }
+        else:
+            result = {
+                'error': 'command not in command list:\n%s' % (pprint.pformat(routing_protocol_client_to_server)),
+            }
+
         self.transport.write(json.dumps(result).encode('latin-1'))
 
 
